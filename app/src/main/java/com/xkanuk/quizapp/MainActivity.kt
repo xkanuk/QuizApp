c2a4f3f6501d709d1dfcd347d9100d3bebf46fa8
Sherman Simpson <xkanuk@gmail.com>
2020-09-22 12:35:45 +1000

    build main UI

--- app/src/main/java/com/xkanuk/quizapp/MainActivity.kt
+++ app/src/main/java/com/xkanuk/quizapp/MainActivity.kt
@@ -1,11 +1,20 @@
+@file:Suppress("DEPRECATION")
+
 package com.xkanuk.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // deprecated, replaced by code in styles.xml
+        // window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
     }
 }
