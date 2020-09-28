b9ec17dd306a80a78c545a659f42e8d7afee699b
Sherman Simpson <xkanuk@gmail.com>
2020-09-28 14:47:34 +1000

    populate question UI

--- app/src/main/java/com/xkanuk/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/xkanuk/quizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.xkanuk.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,20 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionsList = Constants.getQuestions()
         Log.i("Questions Size", "${questionsList.size}")
+
+        val currentPosition = 1
+        val question : Question = questionsList[currentPosition - 1]
+
+        progress_bar.progress = currentPosition
+        tv_progress.text= "$currentPosition/" + progress_bar.max
+
+        tv_question.text = question.question
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+
+
+
     }
 }
